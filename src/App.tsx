9aa5074baaf7edb390b25dabdcc873d750cbac11
Terence <>
2023-11-04 20:22:05 +0900

    feat : basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import RandomSearch from './pages/RandomSearch';
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 600px;
+  overflow: hidden;
+  border-radius: 6px;
+  margin: 0 auto;
+`;
 
 const router = createBrowserRouter([{ path: `*`, element: <RandomSearch /> }]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
+  );
 }
 
 export default App;
